5a0874e6d64ff1b5183903059fd3b4ec2fca641a
Davide-Ettori <ettodavi01@gmail.com>
2024-09-24 17:57:39 -0500

    test files from other repo
    Co-authored-by: angelozangari <angelo.zangari10@gmail.com>

--- main.py
+++ main.py
@@ -172,9 +172,9 @@ def contains(a, b):
     return a.issubset(b)
 
 if __name__ == "__main__":
-    data_file_path = 'data.txt'
-    parameters_file_path = 'parameters.txt'
-    output_file_path = 'out.txt'
+    data_file_path = 'data2/inputdata1.txt'
+    parameters_file_path = 'data2/parameters1.txt'
+    output_file_path = 'data2/out1.txt'
     T, MIS, SDC, M = read_data(data_file_path, parameters_file_path)
 
     '''print(T)
