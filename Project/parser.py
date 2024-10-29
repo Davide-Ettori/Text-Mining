5ae0219292be81afce129c3e28a0c83d5aba5409
angelozangari <angelo.zangari10@gmail.com>
2024-10-29 18:59:05 -0500

    add parser proj

---
+++ Project/parser.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def main():
+    train_path='Project/data/training-Obama-Romney-tweets.xlsx'
+    test_path='Project/data/sample-testdata.xlsx'
+
+    df = pd.read_excel(train_path)
+    print(df.head)
+
+
+    # create list or else for train
+
+    # create list or else for train
+
+    
+
+if __name__ == '__main__':
+    main()
